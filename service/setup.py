15373946dad9be9de92db8cf1f29d7e4d6ce99da
Landon <l.rodden52@gmail.com>
2024-12-28 14:52:45 -0700

    updated links

--- service/setup.py
+++ service/setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='cursion',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     install_requires=[
         'python-dotenv',
